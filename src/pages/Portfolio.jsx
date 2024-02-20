3bf4a53d9d8e7cb2de84f5c9524ab46d3d8cf33d
Amanda Norris <amandakathleennorris@gmail.com>
2024-02-20 17:59:57 -0600

    trying to fix image

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -9,13 +9,13 @@ const Portfolio = () => {
         <Project name={'VANMosaic'} 
           github={'https://github.com/natemcmahon/VANMosaic'}
           deployed={'https://vanmosaic-29816570a7c2.herokuapp.com/'}
-         imageURL={'src/assets/vanmosaic.png'}
+         imageURL={'public/vanmosaic.png'}
           description={'A group project to create an image sharing site using Express, Handlebars, and Firebase'} />
 
         <Project name={'Blog Site'} 
           github={'https://github.com/whimsically/blog-site'}
           deployed={'https://sleepy-reef-35718-76393a7d6bfc.herokuapp.com/'}
-          imageURL={'src/assets/blogsite.png'}
+          imageURL={'public/blogsite.png'}
           description={'A blog with CRUD routes and user authentication. Uses Express, Handlebars, MySQL + Sequelize'} />
 
           <Project name={'Project 3 Placeholder'} 
@@ -27,13 +27,13 @@ const Portfolio = () => {
           <Project name={'Social Media Back End'} 
             github={'https://github.com/whimsically/social-network'}
             deployed={'https://app.screencastify.com/v3/watch/8zJQBXYf3ar6ivklQ6RT'}
-            imageURL={'src/assets/social-backend-ss.png'}
+            imageURL={'public/social-backend-ss.png'}
             description={'Back end for a social media app using Express and Mongoose'} />
           
             <Project name={'NoteTaker'} 
               github={'https://github.com/whimsically/note-taker'}
               deployed={'https://intense-tundra-61951-bbb3cb2d4877.herokuapp.com/'}
-              imageURL={'src/assets/notetaker.png'}
+              imageURL={'public/notetaker.png'}
               description={'An application that lets users save their notes, using Express.js'} />
           
             <Project name={'E-Commerce Back End'} 
