e7aa876f6aa470b3098cd86cdbd9ea12ab5647a9
Amanda Norris <amandakathleennorris@gmail.com>
2024-02-20 01:57:46 -0600

    browser routing, added structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
+import App from './App.jsx'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact.jsx'
+import Projects from './pages/Projects.jsx'
+import Error from './pages/Error'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'contact',
+        element: <Contact />,
+      },
+      {
+        path: 'about',
+        element: <About />,
+      },
+      {
+        path: 'projects',
+        element: <Projects />
+      }
+    ],
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
