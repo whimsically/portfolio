519ea957136fbec0d570db60d2b0a1e4ca7fb714
Amanda Norris <amandakathleennorris@gmail.com>
2024-02-20 18:05:15 -0600

    TRYING TO FIX IMAGES

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,9 +3,9 @@ const About = () => {
       <div>
         <h2>About Me</h2>
         <div className="flexContainer">
-          <img src='src/assets/misa-icon.jpg' alt='Photo of my Cat' className="profile-img"></img>
-          <img src='src/assets/picofme.jpg' alt='Photo of Me' className="profile-img"></img>
-          <img src='src/assets/misa-2.jpg' alt='Another Photo of my Cat' className="profile-img"></img>
+          <img src='/misa-icon.jpg' alt='Photo of my Cat' className="profile-img"></img>
+          <img src='/picofme.jpg' alt='Photo of Me' className="profile-img"></img>
+          <img src='/misa-2.jpg' alt='Another Photo of my Cat' className="profile-img"></img>
           </div>
           <p>
             Hi! I'm Amanda Norris! I started taking a full stack web development bootcamp through KU in September 2023. Since then, I've developed many useful skills and I am looking to start a career.
